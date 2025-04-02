92c7d4a55dc3c1a72a12feeea1fb18262d1f45b3
Nick Gazis <nikolaosgazis3008@gmail.com>
2025-04-02 12:24:47 +0300

    Refactorerd,

--- employee_management.sh
+++ employee_management.sh
@@ -2,9 +2,9 @@
 
 ans=-1 # Initiliazing varible for the 'until' loop.
 
-# Main loop to display menu.
+# Main Loop - Display Menu #
 until [ $ans -eq 5 ]; do
-	# Menu options.
+	# Menu Options #
 	echo "----------------------------------------------------------------"
 	echo "1. Creation of a file with a company's employers information."
 	echo "2. Addition of an employer."
@@ -14,18 +14,18 @@ until [ $ans -eq 5 ]; do
 	echo "[SYSTEM] Choose the action you want:"
 	read ans
 
-	# Validate user input.
+	# Validate user Input #
 	while [[ $ans != 1 && $ans != 2 && $ans != 3 && $ans != 4 && $ans != 5 ]]; do
 		echo "Invalid input. Enter a number from 1 to 5."
 		read ans
 	done
  
-	# Option 1 - Create File.
+	# Option 1 - Create File #
 	if [ $ans -eq 1 ]; then
 		echo "You have chosen to create a new file! How you wish to name it:"
 		read name_of_file
 
-  		# Check if the file already exists.
+  		# Check if the file already exists #
 		while [[ -f "$name_of_file.txt" ]]; do
 			echo "File already exists. Enter an alternate name:"
 			read name_of_file
@@ -35,7 +35,7 @@ until [ $ans -eq 5 ]; do
 		cfile="$name_of_file.txt" # Created file variable.
 		echo "[SYSTEM] File created successfully."
 
-	# Option 2 - Add a new employee.
+	# Option 2 - Add a new Employee #
 	elif [ $ans -eq 2 ]; then
 		echo "[SYSTEM] You have chosen to add a new employee!"
 		echo "[SYSTEM] Enter the password:"
@@ -45,7 +45,7 @@ until [ $ans -eq 5 ]; do
 		echo "[SYSTEM] Enter Name:"
 		read name
   
-		# Input validation.
+		# Input Validation #
 		echo "[SYSTEM] Enter ID:"
 		read id
 	
@@ -68,12 +68,12 @@ until [ $ans -eq 5 ]; do
 		
 		echo "[SYSTEM] File has been updated with new employee's data!"
 	
-	# Option 3 - Search employee by ID.
+	# Option 3 - Search Employee by ID #
 	elif [ $ans -eq 3 ]; then
 		echo "[SYSTEM] You have chosen to search an employee based by ID! Enter their ID:"
 		read search_id
   
-		# Validate ID format.
+		# Validate ID Format #
 		char=$(echo -n "$search_id" | grep -o "[A-Z]" | wc -l)
 		numbers=$(echo -n "$search_id" | grep -o "[0-9]" | wc -l)
 		
@@ -84,18 +84,18 @@ until [ $ans -eq 5 ]; do
 			numbers=$(echo -n "$search_id" | grep -o "[0-9]" | wc -l)
 		done
 		
-		# Search for the employee in the file.
+		# Search for the employee in the file #
 		awk -v search_id="$search_id" '$4 == search_id' $cfile
 	
-	# Option 4 - Show employees from highest to lowest wage.
+	# Option 4 - Show Employees from highest to lowest wage #
 	elif [ $ans -eq 4 ]; then
-		# Saving the respective line's info to temp variables to be able to print it at the end.
+		# Saving the respective line's info to temp variables to be able to print it at the end #
 		awk 'BEGIN{max=-1; min=10000} 
 		$5 > max { max=$5; max_p=$1; max_sur=$2; max_name=$3; max_id=$4; max_chi=$6 } 
 		$5 < min { min=$5; min_p=$1; min_sur=$2; min_name=$3; min_id=$4; min_chi=$6 }
 		END{ print "The employee with the highest wage is:", max_p,max_name,max_sur,max_id,max,max_chi, "\nThe employee with the lowest wage is:", min_p,min_name,min_sur,min_id,min,min_chi}' $cfile
 	
-	# Option 5 - Exit.
+	# Option 5 - Exit #
 	elif [ $ans -eq 5 ]; then
 		echo "You have chosen to exit the program. EXITING!"
 		exit 0
